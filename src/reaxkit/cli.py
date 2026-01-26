041bcb882c8b34d67658575778cca536e36fbd3e
ali <alimohammadi2265@gmail.com>
2026-01-26 14:10:59 -0500

    ffield workflow was developed to get different data from different sections.
    also, an interpreter was added to ffield_analyzer.py to interpret 1-1 to C-C, if C is atom number 1.

--- src/reaxkit/cli.py
+++ src/reaxkit/cli.py
@@ -15,7 +15,7 @@ from reaxkit.workflows import (
     trainset_workflow, fort83_workflow, fort73_workflow,
     electrostatics_workflow, make_video_workflow, plotter_workflow,
     control_workflow, fort76_workflow, fort74_workflow,
-
+    ffield_workflow,
 )
 from reaxkit import introspection
 
@@ -29,7 +29,7 @@ WORKFLOW_MODULES = {
     "fort99": fort99_workflow, "trainset": trainset_workflow, "fort83": fort83_workflow,
     "fort73": fort73_workflow, "elect": electrostatics_workflow, "video": make_video_workflow,
     "plotter": plotter_workflow, "control": control_workflow, "fort76": fort76_workflow,
-    "fort74": fort74_workflow,
+    "fort74": fort74_workflow, "ffield": ffield_workflow,
 }
 
 
