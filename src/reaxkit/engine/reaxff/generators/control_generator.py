efb2e8298bac80ea1f2d906496f08c670e4d882a
ali <alimohammadi2265@gmail.com>
2026-03-13 11:36:53 -0400

    **not finalized** status_workflow was pushed to the branch for checking its functionality in roar collab
    updated control keys in control_generator.py

--- src/reaxkit/engine/reaxff/generators/control_generator.py
+++ src/reaxkit/engine/reaxff/generators/control_generator.py
@@ -41,17 +41,24 @@ CONTROL_TEMPLATE = """# General parameters
       0 icheck     0: Normal run 1:Check first derivatives;2: Single run
       0 idebug     0: normal run 1: debug run
       3 ixmolo     0: only x,y,z-coordinates in xmolout  1: x,y,z + velocities + molnr. in xmolout 2:x,y,z+mol.nr.  3: x,y,z+mol.nr.+Estrain
+10.0000 volcha     volume change (%) with 'S' and 'B' labels
+      0 iconne     0: Normal run 1: Run with fixed connection table 2: Read in from cnt.in
+      0 imolde     0: Normal run 1: Run with fixed molecule definition (moldef.in)
 # MD-parameters
       1 imdmet     MD-method. 1:NVT/Berendsen thermostat 2:do not use;3:NVE 4: NPT/Berendsen thermo/barostat
   0.250 tstep      MD-time step (fs)
 0500.00 mdtemp     MD-temperature
+0500.00 mdtem2     2nd MD-temperature
 0000.00 tincr      Increase/decrease temperature
       2 itdmet     0: T-damp atoms 1: Energy cons 2:System 3: Mols 4: Anderson 5: Mols+2 types of damping
   100.0 tdamp1     1st Berendsen/Anderson temperature damping constant (fs)
+  100.0 tdamp2     2nd Berendsen/Anderson temperature damping constant (fs)
+      0 ntdamp     Nr. of atoms with 1st damping constant and 1st MD-temperature
 0000.00 mdpres     MD-pressure (MPa)
 05000.0 pdamp1     Berendsen pressure damping constant (fs)
       0 inpt       0: Change all cell parameters in NPT-run  1: fixed x 2: fixed y 3: fixed z
 0155000 nmdit      Number of MD-iterations
+ 000000 nmdeqi     Number of MD-equilibrium iterations
   00001 ichupd     Charge update frequency
     025 iout1      Output to unit 71 and unit 73
     100 iout2      Save coordinates
@@ -59,6 +66,7 @@ CONTROL_TEMPLATE = """# General parameters
   00025 itrafr     Frequency of trarot-calls
       1 iout3      0: create moldyn.xxxx-files 1: do not create moldyn.xxxx-files
       1 iravel     1: Random initial velocities
+0.00001 endmd      End point criterium for MD energy minimisation
  025000 iout6      Save velocity file
  000025 irten      Frequency of removal of rotational and translational energy
       0 npreit     Nr. of iterations in previous runs
@@ -78,9 +86,14 @@ CONTROL_TEMPLATE = """# General parameters
       1 igeopt     0: Always use same start gemetries 1:Use latest geometries in optimisation
       0 iincop     heat increment optimisation 1: yes 0: no
 25.0000 accerr     Accepted increase in error force field
+    251 nmoset     Nr. of molecules in training set
 #Outdated parameters
+      0 ideve1     0: Normal run 1:Check for radical/double bond distances
+   2000 ideve2     Frequency of radical/double bond check
       0 nreac      0: reactive; 1: non-reactive; 2: Place default atoms
       1 ibiola     0: output *.geo and *.bgf-files 1: surpress *.geo and *.bgf output files
+100.000 tdhoov     Hoover-Nose temperature damping constant (fs)
+ 01.000 achoov     100*Accuracy Hoover-Nose
       0 itfix      1:Keep temperature fixed at exactly tset
 """
 
